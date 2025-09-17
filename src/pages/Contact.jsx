69e855a584d103e6fbc52474cdf8d33995255733
waleedabdullah472 <waleedkhanii472@gmail.com>
2025-09-17 21:29:56 +0500

    gami and loaction addfd

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -12,13 +12,13 @@ export default function Contect() {
         <div className="contect-info">
           {/* Email */}
           <a
-            href="mailto:support@dreamlandestimation.com"
+            href="crewestimation@gmail.com"
             className="contect-card"
           >
             <img src="/eee.png" alt="Email Icon" className="contect-pic" />
             <div>
               <h3>Email</h3>
-              <p>support@dreamlandestimation.com</p>
+              <p>crewestimation@gmail.com</p>
             </div>
           </a>
 
@@ -27,13 +27,13 @@ export default function Contect() {
             <img src="/lll.png" alt="Location Icon" className="contect-pic" />
             <div>
               <h3>Location</h3>
-              <p>123 Dreamland Plaza, New York, USA</p>
+              <p>Texas, USA</p>
             </div>
           </div>
 
           {/* Phone → WhatsApp */}
           <a
-            href="https://wa.me/15551234567"
+            href="https://wa.me/+17869870481"
             target="_blank"
             rel="noopener noreferrer"
             className="contect-card"
@@ -49,8 +49,8 @@ export default function Contect() {
         {/* 🔹 Right side - Map */}
         <div className="map-container">
           <iframe
-            title="Google Map"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387190.27989433736!2d-74.25986780471538!3d40.69714942288198!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c250b5893ff8c1%3A0xd17e2b18e8a2cba6!2sNew%20York%2C%20USA!5e0!3m2!1sen!2s!4v1694865900000!5m2!1sen!2s"
+            title="Google Map Texas"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d55880454.30297301!2d-130.09765625!3d31.9685989!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640bf3c4fdddfc7%3A0x37aafee3a2a6e3d!2sTexas%2C%20USA!5e0!3m2!1sen!2s!4v1694865900000!5m2!1sen!2s"
             width="100%"
             height="100%"
             style={{ border: 0 }}
