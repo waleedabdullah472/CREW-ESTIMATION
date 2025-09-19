d4c1714cdd9f6acc42bbb1e2a9ee6bef2ce34e63
waleedabdullah472 <waleedkhanii472@gmail.com>
2025-09-19 12:58:06 +0500

    revie comp add

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -47,17 +47,18 @@ export default function Contect() {
         </div>
 
         {/* 🔹 Right side - Map */}
-        <div className="map-container">
-          <iframe
-            title="Google Map Texas"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d55880454.30297301!2d-130.09765625!3d31.9685989!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640bf3c4fdddfc7%3A0x37aafee3a2a6e3d!2sTexas%2C%20USA!5e0!3m2!1sen!2s!4v1694865900000!5m2!1sen!2s"
-            width="100%"
-            height="100%"
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
-          ></iframe>
-        </div>
+       <div className="map-container">
+  <iframe
+    title="Google Map Florida"
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14378862.505994188!2d-94.68627435000002!3d27.994402399999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88dd8c42f66d7c07%3A0x5d12f64e1dfbec8d!2sFlorida%2C%20USA!5e0!3m2!1sen!2s!4v1694865900000!5m2!1sen!2s"
+    width="100%"
+    height="100%"
+    style={{ border: 0 }}
+    allowFullScreen=""
+    loading="lazy"
+  ></iframe>
+</div>
+
       </div>
     </div>
   );
